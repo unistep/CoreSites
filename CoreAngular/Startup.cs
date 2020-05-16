d8772f36cc1ca8389dd23c83366118a72b7fe77a
unistep <avivs@unistep.co.il>
2020-05-16 19:12:39 +0300

    aa

--- CoreAngular/Startup.cs
+++ CoreAngular/Startup.cs
@@ -32,6 +32,7 @@ namespace CoreAngular
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public virtual void ConfigureServices(IServiceCollection services)
 		{
+
 			services.AddSession(options =>
 			{
 				options.IdleTimeout = TimeSpan.FromSeconds(10);
